bf0cc64e5aaf4884f95a38888d0884fbfb8c3154
Daniel-Vono <sand.doomers@gmail.com>
2024-04-29 21:47:55 -0400

    Created a basic grid layout

--- WaveInterferenceAutomaton/Game1.cs
+++ WaveInterferenceAutomaton/Game1.cs
@@ -1,4 +1,8 @@
-﻿using GameUtility;
+﻿// File Name: Game1.cs
+// Creation Date: Apr. 29, 2024
+// Description: The driver class
+
+using GameUtility;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Audio;
 using Microsoft.Xna.Framework.Graphics;
@@ -10,82 +14,133 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 
-namespace WaveInterferenceAutomaton
+/// <summary>
+/// This is the main type for your game.
+/// </summary>
+public class Game1 : Game
 {
-    /// <summary>
-    /// This is the main type for your game.
-    /// </summary>
-    public class Game1 : Game
+
+    private static byte gridWidth = 48;
+    private static byte gridHeight = 32;
+
+    private const int UI_WIDTH = 128;
+    private readonly static int SCREEN_WIDTH = gridWidth * Tile.DIMENSION + UI_WIDTH;
+    private readonly static int SCREEN_HEIGHT = gridHeight * Tile.DIMENSION;
+
+    private GraphicsDeviceManager graphics;
+    private SpriteBatch spriteBatch;
+
+    private Texture2D tileImg;
+
+    private Tile[,] grid;
+
+    public Game1()
     {
-        private GraphicsDeviceManager graphics;
-        private SpriteBatch spriteBatch;
+        graphics = new GraphicsDeviceManager(this);
+        Content.RootDirectory = "Content";
+    }
 
-        public Game1()
+    /// <summary>
+    /// Allows the game to perform any initialization it needs to before starting to run.
+    /// This is where it can query for any required services and load any non-graphic
+    /// related content.  Calling base.Initialize will enumerate through any components
+    /// and initialize them as well.
+    /// </summary>
+    protected override void Initialize()
+    {
+        //Updates and applies a new screen width and height 
+        graphics.PreferredBackBufferWidth = SCREEN_WIDTH;
+        graphics.PreferredBackBufferHeight = SCREEN_HEIGHT;
+        graphics.ApplyChanges();
+
+        base.Initialize();
+    }
+
+    /// <summary>
+    /// LoadContent will be called once per game and is the place to load
+    /// all of your content.
+    /// </summary>
+    protected override void LoadContent()
+    {
+        // Create a new SpriteBatch, which can be used to draw textures.
+        spriteBatch = new SpriteBatch(GraphicsDevice);
+
+        tileImg = new Texture2D(graphics.GraphicsDevice, Tile.DIMENSION, Tile.DIMENSION);
+        Color[] data = new Color[Tile.DIMENSION * Tile.DIMENSION];
+        for (int i = 0; i < data.Length; i++)
         {
-            graphics = new GraphicsDeviceManager(this);
-            Content.RootDirectory = "Content";
+            if(i % Tile.DIMENSION == 0)
+            {
+                data[i] = new Color(0, 0, 0, 255);
+            }
+            else
+            {
+                data[i] = new Color(205, 0, 0, i);
+            }
+            
+        }
+        tileImg.SetData(data);
+
+
+        grid = new Tile[gridHeight, gridWidth];
+        for (byte row = 0; row < gridHeight; row++)
+        {
+            for (byte column = 0; column < gridWidth; column++)
+            {
+                grid[row, column] = new Tile(row, column);
+            }
+        }
+    }
+
+    /// <summary>
+    /// UnloadContent will be called once per game and is the place to unload
+    /// game-specific content.
+    /// </summary>
+    protected override void UnloadContent()
+    {
+        //Unloads the tile image because it was not created using the content manager
+        if (tileImg != null)
+        {
+            tileImg.Dispose();
+            tileImg = null;
+        }
+    }
+
+    /// <summary>
+    /// Allows the game to run logic such as updating the world,
+    /// checking for collisions, gathering input, and playing audio.
+    /// </summary>
+    /// <param name="gameTime">Provides a snapshot of timing values.</param>
+    protected override void Update(GameTime gameTime)
+    {
+        if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+            Exit();
+
+        // TODO: Add your update logic here
+
+        base.Update(gameTime);
+    }
+
+    /// <summary>
+    /// This is called when the game should draw itself.
+    /// </summary>
+    /// <param name="gameTime">Provides a snapshot of timing values.</param>
+    protected override void Draw(GameTime gameTime)
+    {
+        GraphicsDevice.Clear(Color.CornflowerBlue);
+
+        spriteBatch.Begin();
+
+        for (int i = 0; i < gridHeight; i++)
+        {
+            for (int j = 0; j < gridWidth; j++)
+            {
+                spriteBatch.Draw(tileImg, grid[i,j].Rect, Color.White);
+            }
         }
 
-        /// <summary>
-        /// Allows the game to perform any initialization it needs to before starting to run.
-        /// This is where it can query for any required services and load any non-graphic
-        /// related content.  Calling base.Initialize will enumerate through any components
-        /// and initialize them as well.
-        /// </summary>
-        protected override void Initialize()
-        {
-            // TODO: Add your initialization logic here
+        spriteBatch.End();
 
-            base.Initialize();
-        }
-
-        /// <summary>
-        /// LoadContent will be called once per game and is the place to load
-        /// all of your content.
-        /// </summary>
-        protected override void LoadContent()
-        {
-            // Create a new SpriteBatch, which can be used to draw textures.
-            spriteBatch = new SpriteBatch(GraphicsDevice);
-
-            // TODO: use this.Content to load your game content here
-        }
-
-        /// <summary>
-        /// UnloadContent will be called once per game and is the place to unload
-        /// game-specific content.
-        /// </summary>
-        protected override void UnloadContent()
-        {
-            // TODO: Unload any non ContentManager content here
-        }
-
-        /// <summary>
-        /// Allows the game to run logic such as updating the world,
-        /// checking for collisions, gathering input, and playing audio.
-        /// </summary>
-        /// <param name="gameTime">Provides a snapshot of timing values.</param>
-        protected override void Update(GameTime gameTime)
-        {
-            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
-                Exit();
-
-            // TODO: Add your update logic here
-
-            base.Update(gameTime);
-        }
-
-        /// <summary>
-        /// This is called when the game should draw itself.
-        /// </summary>
-        /// <param name="gameTime">Provides a snapshot of timing values.</param>
-        protected override void Draw(GameTime gameTime)
-        {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
-
-            // TODO: Add your drawing code here
-
-            base.Draw(gameTime);
-        }
+        base.Draw(gameTime);
     }
 }
---
+++ WaveInterferenceAutomaton/Tile.cs
@@ -0,0 +1,30 @@
+﻿// File Name: Tile.cs
+// Creation Date: Apr. 29, 2024
+// Description: The generic tile class used to hold partilces in the simulation
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.Xna.Framework;
+
+class Tile
+{
+    public const byte DIMENSION = 8;
+
+    public Rectangle Rect { get;  protected set; }
+
+    protected byte row;
+    protected byte column;
+
+    public Tile(byte row, byte column)
+    {
+        this.row = row;
+        this.column = column;
+
+        Rect = new Rectangle(column * DIMENSION, row * DIMENSION, DIMENSION, DIMENSION);
+    }
+}
+
